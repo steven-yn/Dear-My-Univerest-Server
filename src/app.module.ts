540f0a27112b5ac8d19621c7cc0cb7fac08b2831
steven-yn <ssungyeon94@gmail.com>
2023-05-27 16:45:55 +0900

    add: apidoc 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve('./', 'apidocs'),
+      serveRoot: '/apidocs',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,36 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  /**
+   * @api {get} / getHello
+   * @apiName getHello
+   * @apiGroup Test
+   *
+   * @apiSuccess {String} msg Hello World! 메시지를 응답해요.
+   *
+   * @apiParam {Null} none "아무것도 보내지 않아도 되요."
+   *
+   * @apiBody {Null} none "아무것도 보내지 않아도 되요."
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "msg": "Hello World!"
+   *     }
+   *
+   * @apiError NotFound 알수없는 요청
+   *
+   * @apiErrorExample Error-Response:
+   *     HTTP/1.1 404 Not Found
+   *     {
+   *       "error": "error"
+   *     }
+   */
+  getHello(): {
+    msg: string;
+  } {
+    return {
+      msg: 'Hello World!',
+    };
   }
 }
